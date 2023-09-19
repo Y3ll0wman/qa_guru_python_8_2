87dc597ac841c6ac5be9f34cf1c4ae11e665f99b
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-09-19 14:22:26 +0300

    Вынес browser.open из сэтапа в тело теста

--- test_search_in_google.py
+++ test_search_in_google.py
@@ -2,11 +2,13 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_google_should_find_selene(open_google):
+def test_google_should_find_selene():
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
 
 
-def test_search_returns_should_be_no_results(open_google):
+def test_search_returns_should_be_no_results():
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('eolfdk;l77777gidjxcznb44444hdkjasdhf888ghkl').press_enter()
     browser.element('[class="card-section"]').should(have.text('ничего не найдено'))
